5cf1ebba52c489367a392db4fcecfd11916165f2
rami jawadi <ramijawadi104@gmail.com>
2023-10-04 17:18:31 +0100

    create home route and update index.ejs

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
 require('dotenv').config()
-
+const articlesRouter = require('./Routes/article')
 const app = express()
+
+
+
+app.use('/articles' , articlesRouter);
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.render('index')
+
+const articles = [{
+
+    title : 'test title of the articles', 
+    createdAt : new Date(),
+    description : 'test description of the articles'
+}, 
+
+{
+
+    title : 'test title of the articles 2', 
+    createdAt : new Date(),
+    description : 'test description of the articles 2'
+},
+
+]
+
+    res.render('articles/index' , {articles :articles})
 })
 
 
