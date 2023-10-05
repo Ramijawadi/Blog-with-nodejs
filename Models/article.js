1c1680ebea326813abbea396f190a52fe964b0fa
rami jawadi <ramijawadi104@gmail.com>
2023-10-05 15:15:34 +0100

    FEATUERE : add dompurify and JSDOM libraries to read the HTML

--- Models/article.js
+++ Models/article.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const slug = require("slugify");
 const marked = require("marked");
-const { default: slugify } = require("slugify");
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+
+
+const dompurify = createDomPurify(new JSDOM().window)
 
 const acticleSchema = new mongoose.Schema({
   title: {
@@ -25,11 +29,22 @@ const acticleSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+
+sanitizeHtml : {
+    type: String ,
+    required : true
+}
+
 });
 
 acticleSchema.pre("validate", function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  if(this.markdown) {
+
+    this.sanitizeHtml = dompurify.sanitize(marked(this.markdown))
   }
   next();
 });
