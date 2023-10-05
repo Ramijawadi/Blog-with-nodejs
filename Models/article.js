cabe61233c045ce23df526f9cf9d3e0fb97ea74c
rami jawadi <ramijawadi104@gmail.com>
2023-10-05 17:41:40 +0100

    FIX : Update functionality + refactoring code

--- Models/article.js
+++ Models/article.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
-const slug = require("slugify");
-const marked = require("marked");
+const slugify = require("slugify");
+const {marked} = require('marked');
 const createDomPurify = require('dompurify')
 const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window);
 
 
-const dompurify = createDomPurify(new JSDOM().window)
-
-const acticleSchema = new mongoose.Schema({
+const articleSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
   },
-  descrption: {
+  description: {
     type: String,
   },
   markdown: {
@@ -35,18 +34,18 @@ sanitizeHtml : {
     required : true
 }
 
-});
+})
 
-acticleSchema.pre("validate", function (next) {
+articleSchema.pre('validate', function(next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
-  }
+  } 
 
   if(this.markdown) {
 
     this.sanitizeHtml = dompurify.sanitize(marked(this.markdown))
-  }
-  next();
+  } 
+  next()
 });
 
-module.exports = mongoose.model("Article", acticleSchema);
+module.exports = mongoose.model("Article", articleSchema);
--- server.js
+++ server.js
@@ -1,43 +1,38 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose');
-const articlesRouter = require('./Routes/article')
-const Article =require('./Models/article')
-const methodeOverride = require('method-override')
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const articlesRouter = require("./Routes/article");
+const Article = require("./Models/article");
+const methodOverride = require("method-override");
+const app = express();
 
-app.use(express.urlencoded({ extended: false }))
-app.use(methodeOverride('_method'))
 
-app.set('view engine', 'ejs')
-app.get('/', async (req, res) => {
 
-   const articles = await  Article.find().sort({createdAt : 'desc'})
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
 
-    res.render('articles/index', { articles: articles })
-})
-
+  res.render("articles/index", { articles: articles });
+});
 
 //pour tt les routes sous /articles
-app.use('/articles', articlesRouter);
-
-
+app.use("/articles", articlesRouter);
 
 const uri = process.env.MONGO_URI;
 mongoose
-    .connect(uri , {
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 
-useNewUrlParser : true , useUnifiedTopology: true 
+  .then(() => {
+    console.log(`db connected successfully`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-    })
-
-    .then(() => {
-        console.log(`db connected successfully`);
-   
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-const port = process.env.PORT
-app.listen(port, console.log(`app conneted at port ${port}`))
+const port = process.env.PORT;
+app.listen(port, console.log(`app conneted at port ${port}`));
